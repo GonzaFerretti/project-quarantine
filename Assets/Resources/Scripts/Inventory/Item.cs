f35be9bfaa41209b486abea322a1e43a1d46cfd8
Gonza Ferretti <gonzalofe1998@hotmail.com>
2020-04-06 19:50:44 -0300

    pre test merge arte

---
+++ Assets/Resources/Scripts/Inventory/Item.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public int id;
+    public string displayName;
+    public string description;
+    public Item()
+    {
+        public int id;
+    }
+}
