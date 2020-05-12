15fbe3da20c47e1a83fce67c87a57a0ad1492cd9
Gonza Ferretti <gonzalofe1998@hotmail.com>
2020-05-12 02:01:08 -0300

    removed crap

--- Assets/Resources/Scripts/Actions/Scripts/Player/MoventDirections.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[CreateAssetMenu(menuName = "Controller/Action/Movement Direction")]
-public class movementVector : ScriptableObject
-{
-    public movementKeysDirection keysDirection;
-    public Vector3 directionVector;
-}
